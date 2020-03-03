1c9c06f8043c946414ba60edd5c901911e09602d
su8lim3kid <aldereteangel21@gmail.com>
2020-03-03 13:16:31 -0700

    fixed rails routes and added basic components

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, } from 'react';
+import { Route, Switch, } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import NoMatch from './components/NoMatch.js';
+import { Container, } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Fragment>
+    <Container>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Container>
+  </Fragment>
+);
 
 export default App;
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
     resources :customers do
       resources :items
     end
+  end
 end
