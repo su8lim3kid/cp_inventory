2f5eb6753b6b5a394694331319f97363ca3387fb
su8lim3kid <aldereteangel21@gmail.com>
2020-03-03 10:56:39 -0700

    item controller set up

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,6 +1,50 @@
 class Api::ItemsController < ApplicationController
-
+before_action :set_customer
+before_action :set_item, only: [:show, :update, :destroy]
 
   def index
-    
+    render json: @customer.items
+  end
+
+  def show
+    render json: @items
+  end
+
+  def create
+    item = @customer.items.new(item_params)
+
+    if item.save
+      render json:item
+    else
+      render json: item.errors, status: 422
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      render json: @item
+    else 
+      render json: @item.errors, status: 422
+    end
+  end
+
+  def destroy
+    @item.destroy
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def set_customer
+    @customer = Customer.find(params[:customer_id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :quantity, :pallets)
+  end
+
+
 end
